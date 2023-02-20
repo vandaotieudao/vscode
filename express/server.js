35fabc7055b584cafa17880672531af3f44a3b3b
anh1901040026 <98082469+anh1901040026@users.noreply.github.com>
2023-02-20 23:07:46 +0700

    connect Mongodb

--- express/server.js
+++ express/server.js
@@ -1,6 +1,27 @@
 var express = require("express");
-var app = express();
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        await mongoose.connect('mongodb+srv://root:root@cluster0.gyhxn3j.mongodb.net/cluster0?retryWrites=true&w=majority',{
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        console.log("DB connect")
+    }catch(error){
+        console.log("error")
+        process.exit(1)
+
+    }
+}
+
+connectDB();
+
+const app = express();
+app.get('/', (res, rep) => res.setEncoding('Hello'))
 app.listen(3000, () => {
     console.log("Created ");
 });
